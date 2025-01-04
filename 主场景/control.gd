41503fba7ebfd7f2ecfbceb12a8be056d9a37153
gaoyan2659365465 <2659365465@qq.com>
2025-01-04 21:41:02 +0800

    4

--- 主场景/control.gd
+++ 主场景/control.gd
@@ -7,3 +7,5 @@ func _process(delta: float) -> void:
 func _draw() -> void:
 	var default_font = ThemeDB.fallback_font
 	draw_string(default_font, Vector2(500,500), "Hello World", HORIZONTAL_ALIGNMENT_CENTER, -1, 13,Color("#c3c3c3"))
+	
+	draw_arc(Vector2(400,400),50,0.0,2.0,20,Color(0.045, 0, 1),200,true)
--- 主场景/node_2d.gd
+++ 主场景/node_2d.gd
@@ -3,9 +3,12 @@ extends Node2D
 
 
 
-
-
 func _draw() -> void:
+	print("123456")
 	draw_line(Vector2(0,0),Vector2(100,100),Color(1, 0, 0),2.0,true)
+	
+	var default_font = ThemeDB.fallback_font
+	draw_string(default_font, Vector2(50,50), "Hello", HORIZONTAL_ALIGNMENT_CENTER, -1, 30,Color("#c3c3c3"))
+
 	
 	
